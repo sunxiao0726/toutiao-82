6301803ea8b929b75575d0444990372fce6e1306
sunxiao0726 <940629124@qq.com>
2019-08-31 22:23:24 +0800

    初始化项目-页面分析-新建登录和主页

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/home'
+import Login from './views/login'
 
 Vue.use(Router)
 
@@ -10,6 +11,9 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    }, {
+      path: '/login',
+      component: Login
     }
     // {
     //   path: '/about',
