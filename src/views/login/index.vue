1807a5d965b3d85849765a8443537d1c7be861da
sunxiao0726 <940629124@qq.com>
2019-09-01 21:11:54 +0800

    登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,19 +4,20 @@
             <div class="logo">
                 <img src="../../assets/img/logo_index.png" alt="">
             </div>
-            <el-form style='margin-top:20px'>
-                <el-form-item>
-                    <el-input></el-input>
+            <el-form style='margin-top:20px' :model="loginForm" :rules="loginRules" ref="loginForm">
+
+                <el-form-item prop="mobile">
+                    <el-input placeholder="请输入手机号" v-model="loginForm.mobile"></el-input>
                 </el-form-item>
-                <el-form-item>
-                    <el-input style="width:250px"></el-input>
+                <el-form-item prop="code">
+                    <el-input style="width:250px" placeholder="请输入验证码" v-model="loginForm.code"></el-input>
                      <el-button style="float:right">发送验证码</el-button>
                 </el-form-item>
-                <el-form-item>
-                    <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+                <el-form-item prop="check">
+                    <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary" style='width:100%'>登录</el-button>
+                    <el-button @click="login(loginForm)" type="primary" style='width:100%'>登录</el-button>
                 </el-form-item>
             </el-form>
         </el-card>
@@ -26,8 +27,51 @@
 
 <script>
 export default {
+  data () {
+    let validator = function (rule, value, callback) {
+      if (value) {
+        callback()
+      } else {
+        callback(new Error('您必须无条件被坑'))
+      }
+    }
+
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        check: false// 是不是同意条款
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { len: 11, message: '手机长度必须为11个字符', trigger: 'blur' },
+          { pattern: /^1[3456789]\d{9}$/, message: '手机号格式错误', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { pattern: /^\d{6}$/, message: '密码长度必须为6个数字', trigger: 'blur' }
+        ],
+        check: [
+          { validator }
+        ]
+
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate(isOk => {
+        if (isOk) {
+          console.log(isOk)
+        }
+      })
+    }
+
+  }
 
 }
+
 </script>
 
 <style lang="less" scoped>
