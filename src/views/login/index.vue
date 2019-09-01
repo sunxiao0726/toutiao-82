c5a9e987251428e5dfa35463fff83f1675d21f18
sunxiao0726 <940629124@qq.com>
2019-09-01 08:46:01 +0800

    登录模块-页面布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,27 @@
 <template>
-<div>
-    login
-</div>
+    <div class="login">
+        <el-card class="login-car">
+            <div class="logo">
+                <img src="../../assets/img/logo_index.png" alt="">
+            </div>
+            <el-form style='margin-top:20px'>
+                <el-form-item>
+                    <el-input></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-input style="width:250px"></el-input>
+                     <el-button style="float:right">发送验证码</el-button>
+                </el-form-item>
+                <el-form-item>
+                    <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" style='width:100%'>登录</el-button>
+                </el-form-item>
+            </el-form>
+        </el-card>
+
+    </div>
 </template>
 
 <script>
@@ -10,6 +30,24 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.login {
+    background-image : url('../../assets/img/login_bg.jpg');
+    height: 100vh;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-car{
+        width: 450px;
+        height: 350px;
+        .logo {
+            text-align: center;
+            img {
+                height: 40px;
+            }
+        }
+    }
+}
 
 </style>
