d7c64948a78d7ace9ca3c0c73c58d179e533eeb7
sunxiao0726 <940629124@qq.com>
2019-09-06 21:15:16 +0800

    评论列表-页面结构-table的使用

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,9 +1,41 @@
 <template>
-<div>评论路由</div>
+<el-card>
+    <bread-crumb slot="header">
+        <template slot="title">评论列表</template>
+    </bread-crumb>
+    <el-table :data="list" >
+      <el-table-column
+        prop="date"
+        label="标题"
+        width="700">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="评论状态">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="总评论数">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="粉丝评论数">
+      </el-table-column>
+        <el-table-column
+        prop="address"
+        label="操作">
+      </el-table-column>
+    </el-table>
+</el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      list: []
+    }
+  }
 
 }
 </script>
