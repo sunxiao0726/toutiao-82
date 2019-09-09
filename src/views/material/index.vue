1e9859da269b52e0a45d2d034715f7be8a5e036e
sunxiao0726 <940629124@qq.com>
2019-09-09 21:48:01 +0800

    素材列表-新建页面-挂载路由,素材列表-页面结构-tab

---
+++ src/views/material/index.vue
@@ -0,0 +1,29 @@
+<template>
+   <el-card>
+        <bread-crumb slot="header">
+            <!-- 面包屑的具名插槽 -->
+            <template slot="title">素材管理</template>
+        </bread-crumb>
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+          <el-tab-pane label="全部素材" name="first">
+          </el-tab-pane>
+          <el-tab-pane label="收藏图片" name="second">
+          </el-tab-pane>
+        </el-tabs>
+   </el-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      activeName: 'first'
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
