bddb2eaab9e1cf5511c512a05f4523f37209135e
sunxiao0726 <940629124@qq.com>
2019-09-10 21:37:06 +0800

    0910

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,12 +2,12 @@
   <div>
       <el-container>
         <!-- 左侧导航 -->
-        <el-aside class="aside" style="width:201px">
+        <el-aside class="aside" style="width:201px;background-color:#323745">
           <layout-aside></layout-aside>
         </el-aside>
 
         <!-- 右侧大盒子 -->
-        <el-container style="height=100vh,overflow:hidden">
+        <el-container style="overflow:hidden">
           <!-- 头部 -->
           <el-header>
             <layout-header></layout-header>
@@ -28,5 +28,5 @@ export default {
 </script>
 
 <style>
-
+background-color: #fff;
 </style>
--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,15 +1,35 @@
 <template>
-   <el-card>
+   <el-card class="material">
         <bread-crumb slot="header">
-            <!-- 面包屑的具名插槽 -->
             <template slot="title">素材管理</template>
         </bread-crumb>
-        <el-tabs v-model="activeName" @tab-click="handleClick">
-          <el-tab-pane label="全部素材" name="first">
+        <el-tabs v-model="activeName" @tab-click="changeTab">
+          <el-tab-pane label="全部素材" name="all">
+            <div class="card-list">
+              <el-card v-for="item in list" :key="item.id" class="img-card">
+                <img :src="item.url" alt="">
+                <el-row type="flex" justify="space-around" class="operate" align="middle">
+                  <i class="el-icon-star-on" :style="{color: item.is_collected ? 'red' : ''}"></i>
+                  <i class="el-icon-delete-solid"></i>
+                </el-row>
+              </el-card>
+            </div>
           </el-tab-pane>
-          <el-tab-pane label="收藏图片" name="second">
+          <el-tab-pane label="收藏图片" name="collect">
+            <div class="card-list">
+              <el-card v-for="item in list" :key="item.id" class="img-card">
+                <img :src="item.url" alt="">
+              </el-card>
+            </div>
           </el-tab-pane>
         </el-tabs>
+        <el-row>
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="1000">
+          </el-pagination>
+        </el-row>
    </el-card>
 </template>
 
@@ -17,13 +37,59 @@
 export default {
   data () {
     return {
-      activeName: 'first'
+      activeName: 'all',
+      list: []
     }
+  },
+  methods: {
+    // 切换页签
+    changeTab () {
+      this.getMaterial()
+    },
+    getMaterial () {
+      this.$axios({
+        url: '/user/images',
+        params: {
+          collect: this.activeName === 'collect'
+        }
+      }).then(res => {
+        this.list = res.data.results
+      })
+    }
+  },
+  created () {
+    this.getMaterial()
   }
-
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.material{
+  .card-list {
+    display:flex;
+    flex-wrap : wrap;
+    justify-content: center;
+    .img-card{
+      width: 180px;
+      height: 180px;
+      margin: 30px;
+      position: relative;
+      overflow: hidden;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+      .operate {
+        position: absolute;
+        width: 100%;
+        bottom: 0;
+        left:0;
+        height: 30px;
+        background-color: #f4f5f6;
+        font-size: 18px;
+      }
+    }
+  }
+}
 
 </style>
